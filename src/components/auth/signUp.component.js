2db8fc0eadcc58efa65c7741c9321daf2cd4a851
Nhlakanipho <nhlaka.devv@gmail.com>
2021-08-07 17:25:21 +0200

    adding the sign up method

--- src/components/auth/signUp.component.js
+++ src/components/auth/signUp.component.js
@@ -3,7 +3,7 @@ import ConfirmAccount from './confirmAccount.component';
 import { Formik, Form, Field } from 'formik';
 import AuthAPI from '../../api/auth';
 
-const FormikSignUp = () => (
+const FormikSignUp = (props) => (
     <Formik
       initialValues={{
         username: '',
@@ -11,9 +11,14 @@ const FormikSignUp = () => (
         password: '',
         confirmPassword: ''
       }}
-      onSubmit={async (values) => {
-        // perform operations
-        console.log(values);
+      onSubmit={async ({username, email, password}) => {        
+        try {
+            const createUserAccount = await AuthAPI.signUp(username, password, email);
+            return props.userCreated(true, createUserAccount.username);
+        } catch(error){
+            // handle error!
+            console.log(error);
+        }
       }}
     >
     {({ isSubmitting }) => (
@@ -30,39 +35,28 @@ const FormikSignUp = () => (
                 <div>
                     <label htmlFor="password">Password</label>
                     <Field type="password" name="password" placeholder="Password123"/>
-                    <ul>
-                        <li>Password must at least have 8 characters</li>
-                        <li>Include at least one capital character</li>
-                        <li>Include at least one lowercase character</li>
-                        <li>Include at least one number</li>
-                    </ul>
-                </div>
-                <div>
-                    <label htmlFor="confirm password">Confirm Password</label>
-                    <Field type="confirm password" name="confirm password" placeholder="Password123"/>
-                    <ul>
-                        <li>Password does not match</li>
-                    </ul>
                 </div>
                 <button type="submit" disabled={ isSubmitting }>Continue</button>
             </Form>
         </div>
     )}
-
     </Formik>
 );
 
 function SignUp(props){
     const [user, setUser] = useState(false);
+    const [authenticatedUsername, setAuthenticatedUsername] = useState('');
 
-    function userCreated(complete){
+    function userCreated(complete, username){
+        setAuthenticatedUsername(username);
+
         if (complete){
             setUser(true);
         }
     }
 
     if (user){
-       return <ConfirmAccount confirmAuthentication={props.confirmAuthentication}/>
+       return <ConfirmAccount confirmAuthentication={ props.confirmAuthentication } username={authenticatedUsername}/>
     } else {
         return (
             <>
