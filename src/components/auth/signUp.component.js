72eaceac52e46c7af9c7ac27ff3ee643e7dbbba3
Nhlakanipho <nhlaka.devv@gmail.com>
2021-08-07 15:50:17 +0200

    configuring formik on the sign up component

--- src/components/auth/signUp.component.js
+++ src/components/auth/signUp.component.js
@@ -1,62 +1,59 @@
 import { useState } from 'react';
 import ConfirmAccount from './confirmAccount.component';
-import { useFormik } from 'formik';
+import { Formik, Form, Field } from 'formik';
+import AuthAPI from '../../api/auth';
 
-            // <div>
-            //     <h1>SignUp Component</h1>
-            //     <button onClick={() => props.toggle("signIn")}>Already a member</button>
-            //     <button onClick={() => setUser(true)}>Sign Up</button>
-            // </div>
-    
+const FormikSignUp = () => (
+    <Formik
+      initialValues={{
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }}
+      onSubmit={async (values) => {
+        // perform operations
+        console.log(values);
+      }}
+    >
+    {({ isSubmitting }) => (
+        <div>
+            <Form>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <Field name="username" placeholder="codeNhlaka"/>
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <Field name="email" placeholder="codeNhlaka@gmail.com"/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <Field type="password" name="password" placeholder="Password123"/>
+                </div>
+                <div>
+                    <label htmlFor="confirm password">Confirm Password</label>
+                    <Field type="confirm password" name="confirm password" placeholder="Password123"/>
+                </div>
+                <button type="submit" disabled={ isSubmitting }>Continue</button>
+            </Form>
+        </div>
+    )}
 
-function FormikSignUp(props){
-    async function handleSignIn(username, password, email, confirmPassword){
-        let user;
-        if (username && password && email && confirmPassword){
-            return props.confirmAuthentication();
-        }
-    }
-
-    const formik = useFormik({
-        initialValues: {
-            username: '',
-            email: '',
-            password: '',
-            confirmPassword: ''
-          },
-        onSubmit: values => {
-            const {username, email, password, confirmPassword} = values;
-            return handleSignIn(username, password, email, confirmPassword);
-        }
-    })
-
-    return (
-        <form onSubmit={formik.handleSubmit}> 
-            <div>
-                <label htmlFor="username">Username</label>
-                <input type="text" value={formik.values.username} name="username" onChange={formik.handleChange} placeholder="JohnDoe" maxLength="12"/>
-            </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" value={formik.values.email} name="email" onChange={formik.handleChange} placeholder="Email" />
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" value={formik.values.password} name="password" onChange={formik.handleChange} placeholder="Password123@" maxLength="14"/>
-            </div>
-            <div>
-                <label htmlFor="confirm password">Confirm Password</label>
-                <input type="password" value={formik.values.password} name="confirm password" onChange={formik.handleChange} placeholder="Password132@" maxLength="14"/>
-            </div>
-            <button type="submit">Continue</button>
-        </form>
-    )
-}
+    </Formik>
+);
 
 function SignUp(props){
     const [user, setUser] = useState(false);
+
+    function userCreated(complete){
+        if (complete){
+            setUser(true);
+        }
+    }
+
     if (user){
-       return <ConfirmAccount/>
+       return <ConfirmAccount confirmAuthentication={props.confirmAuthentication}/>
     } else {
         return (
             <>
@@ -67,10 +64,10 @@ function SignUp(props){
                     <h3>Sign Up</h3>
                     <button onClick={e => {
                         e.preventDefault();
-                        props.toggle("signUp")
+                        props.toggle("signIn")
                     }}>Already a member</button>
 
-                    <FormikSignUp confirmAuthentication={props.confirmAuthentication}/>
+                    <FormikSignUp userCreated={userCreated}/>
                 </div>
             </div>
             </>
