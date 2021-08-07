7045e366880da63c1c87768d3e9e3e84d6959d8f
Nhlakanipho <nhlaka.devv@gmail.com>
2021-08-08 01:08:49 +0200

    working the Forgot Password component

--- src/components/auth/forgotPassword.component.js
+++ src/components/auth/forgotPassword.component.js
@@ -1,26 +1,76 @@
 import { useState } from 'react';
+import {Formik, Form, Field } from 'formik';
+
+const FormikNewPassword = () => (
+    <Formik
+        initialValues={{
+            code: '',
+            newPassword: ''
+        }}
+        onSubmit={async ({code, newPassword}) => {
+            console.log(code, newPassword);
+        }}
+    >
+        {({isSubmitting}) => (
+            <Form>
+                <div>
+                    <label htmlFor="code">Verification</label>
+                    <Field name="code" placeholder="Verification code"/>
+                </div>
+                <div>
+                    <label htmlFor="newPassword">New Password</label>
+                    <Field name="newPassword" type="password" placeholder="Password1234"/>
+                </div>
+                <button type="submit" disabled={isSubmitting}>Continue</button>
+            </Form>
+        )}
+    </Formik>
+)
 
 function SetNewPassword(){
-    function confirmNewPassword(){
-        // confirm new password then sign user in
-        alert('SUCCESS!')
-    }
-
     return (
         <div>
             <h1>Set New Password Component</h1>
-            <button onClick={() => confirmNewPassword()}>Change Password</button>
+            <FormikNewPassword/>
         </div>
     )
 }
 
+const FormikForgotPassword = (props) => (
+    <Formik
+        initialValues= {
+            {
+                username: ''
+            }
+        }
+        onSubmit={async ({username}) => {
+            // make necesssary api calls
+            props.confirmVerification();
+        }}
+    >
+        {({isSubmitting}) => ( 
+            <Form>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <Field name="username" placeholder="codeNhlaka"/>
+                </div>
+                <button type="submit" disabled={isSubmitting}>Continue</button>
+            </Form>
+        )}
+    </Formik>
+)
+
 function ForgotPassword(){
-    const [validatedAccount, setValidatedAccount] = useState(false); 
-    if (!validatedAccount){
+    const [accountVerificationComplete, setAccountVerificationComplete] = useState(false); 
+    function verificationConfirmation(){
+        return setAccountVerificationComplete(true);
+    }
+
+    if (!accountVerificationComplete){
         return (
             <div>
-                <h1>Forgot Password Component</h1>
-                <button onClick={() => setValidatedAccount(true)}>Confirm and Set New Password</button>
+                <h1>Reset Password</h1>
+                <FormikForgotPassword confirmVerification={verificationConfirmation}/>
             </div>
         )
     } else return <SetNewPassword/>
