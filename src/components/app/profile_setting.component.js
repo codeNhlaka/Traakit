bfcc111989803cf840e2901d9c8b6421406d48ff
Nhlakanipho <nhlaka.devv@gmail.com>
2021-08-12 14:30:24 +0200

    removing react spring

--- src/components/app/profile_setting.component.js
+++ src/components/app/profile_setting.component.js
@@ -1,36 +1,38 @@
 import { useState, useContext } from "react";
 import { profileSettingsContext } from "../../appContext";
 import { useDrag } from "react-use-gesture";
-import { useSpring, animated } from "react-spring";
 
 function ProfileSettingsComponent(props){
-    
-    const componentPosition = useSpring({
-        x: 400,
+    const [componentPosition, setComponentPosition] = useState({
+        x: 100,
         y: 100
     });
 
     // drag component
     const bindPosition = useDrag((params) => {
-        componentPosition.x.set(params.offset[0]);
-        componentPosition.y.set(params.offset[1]);
+        setComponentPosition({
+            x: params.offset[0],
+            y: params.offset[1]
+        })
     });
 
-    // hide component
+    // hide component context
     const toggleProfileSettings = useContext(profileSettingsContext);
+
+    
     
     return (
-        <animated.div {...bindPosition()} className="container absolute transition shadow-lg bg-white ml-auto h-4/5 rounded w-1/3"
+        <div {...bindPosition()} className="container absolute transition shadow-lg bg-white ml-auto h-4/5 rounded w-1/3"
             style={
                 {
-                    y: componentPosition.y,
-                    x: componentPosition.x,
+                    top: componentPosition.y,
+                    left: componentPosition.x,
                     zIndex: 1000
                 }
             }
         >
             <div className="cursor-pointer mt-10 w-auto" onClick={() => toggleProfileSettings() }>close</div>
-        </animated.div>
+        </div>
     )
 }
 
