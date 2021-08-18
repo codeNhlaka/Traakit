37a42bc861d1c12adbb670d26630451382da31c6
Nhakanipho <86412371+codeNhlaka@users.noreply.github.com>
2021-08-18 14:55:37 +0200

    Update amplifyconfig.js

--- src/amplifyconfig.js
+++ src/amplifyconfig.js
@@ -1,6 +1,6 @@
 const config = {
-  userPoolId: "us-east-2_xWiFTqL6c",
-  clientId: "5euk7816tcmjs79706amgvars0",
+  userPoolId: "<PoolID>",
+  clientId: "<ClientID>",
   globalRegion: "us-east-2",
 };
 
