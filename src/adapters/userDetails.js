fb9da403e3ee2d31a9a6a534450f9997c123ff22
Nhlakanipho <nhlaka.devv@gmail.com>
2021-08-25 17:18:44 +0200

    updating the userDetails adapter

--- src/adapters/userDetails.js
+++ src/adapters/userDetails.js
@@ -1,15 +1,5 @@
 import Amplify, { Storage } from "aws-amplify";
 
 export class UserDetailsAPI {
-    static async setUserImage(file, key){
-        let result;
 
-        try {
-            result = await Storage.put(key, file);
-            console.log(result);
-            return true;
-        } catch(error){
-            console.log(error);
-        }
-    }
 }
