ea3f53c95797c5c2fc404b323eec9fbd71370fb6
Nhlakanipho <nhlaka.devv@gmail.com>
2021-08-29 23:44:49 +0200

    working on the `Documents` container

--- src/containers/documents.js
+++ src/containers/documents.js
@@ -1,6 +1,8 @@
 import DocumentsComponent from "../components/documents/documents.component";
 import { Storage } from "aws-amplify";
 import { useEffect } from "react";
+import { useStore } from "../store/store";
+import { v4 as uuidv4 } from "uuid";
 
 // config storage
 
@@ -11,12 +13,22 @@ Storage.configure({
 });
 
 function Documents(){
-    useEffect(() => {
+    const user = useStore(state => state.about);
+    const { id } = user;
 
-        function processDocument(doc){
-            // do something
-            console.log(doc)
-        }
+    async function deleteDoc(documentID){
+
+    }
+
+    async function downloadDoc(documentID){
+
+    }
+
+    async function uploadDoc(document){
+        
+    }
+
+    useEffect(() => {
 
         // fetch user documents
         const documentsList = Storage.list('').then( docs => {
@@ -27,7 +39,11 @@ function Documents(){
         })
         
     }, []);
-    return <DocumentsComponent/>
+    return <DocumentsComponent
+        uploadDocument={ uploadDoc }
+        deleteDocument={ deleteDoc }
+        downloadDocument={ downloadDoc }
+    />
 }
 
 export default Documents;
