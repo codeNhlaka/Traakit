f29d0fcf3a756803916555fec38bb6b8a028d379
Nhlakanipho <nhlaka.devv@gmail.com>
2021-08-30 19:58:34 +0200

    fixing bugs

--- src/store/store.js
+++ src/store/store.js
@@ -18,8 +18,10 @@ const initialState = {
 export const useStore = create(set => ({
     about: initialState,
     deleteDocumentRecord: (id) => set(state => ({
-        ...state.about, 
-        documents: state.documents.filter(doc => doc.id !== id)
+        about: {
+            ...state.about, 
+            documents: state.about.documents.filter(doc => doc.id !== id)
+        }
     })), 
     setDocumentRecord: (record) => set(state => ({
         about: {
