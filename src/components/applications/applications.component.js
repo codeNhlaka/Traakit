1f2273260b90b39633658ca9b7a7ccda348661f5
Nhlakanipho <nhlaka.devv@gmail.com>
2021-08-31 22:15:39 +0200

    :hankey: working on the `Application Component`

--- src/components/applications/applications.component.js
+++ src/components/applications/applications.component.js
@@ -1,17 +1,87 @@
-import { useState, useContext} from "react";
-import { useDrag } from "react-use-gesture";
+import { useState, useContext, useEffect} from "react";
+import { ApplicationsProvider, ApplicationsContext } from "../../context/applications";
 import Navigation from "../routes/nagivation.component";
 import ExportIcon from "../../assets/icons/export.icon";
 import FilterIcon from "../../assets/icons/filter.icon";
 import PlusIcon from "../../assets/icons/plus.icon";
 import { applicationDetailsContext } from "../../context/appContext";
 import Select from "../form/dropdown.component";
-import InputField, { ContainedInputField } from "../form/input.component";
+import { ContainedInputField } from "../form/input.component";
+import { useStore } from "../../store/store";
+import DeleteIcon from "../../assets/icons/delete.icon";
 
-function ApplicationDetails(){
+function CreateApplication({ toggleCreateApp }){
+    const { createApp } = useContext(ApplicationsContext);
+
+    const [application, setApplication] = useState({
+        companyName: null,
+        role: null,
+        progress: "pending"
+    });
+
+
+    function handleChange(forInput, value){
+        let property;
+
+        if (forInput === "company name") forInput = "companyName";
+
+        property = forInput;
+
+        setApplication({
+            ...application,
+            [property] : value
+        });
+    }
+
+    return (
+        <div className="border border-gray-800 absolute shadow-md bg-coolgray container top-32 left-96 w-96 rounded h-64 z-50">
+            <div className="mt-5 content w-full cursor-default h-72">
+                <ContainedInputField 
+                    handleChange={ handleChange } 
+                    placeholder="Google"
+                    inputLabel="Company Name"
+                />
+
+                <ContainedInputField 
+                    handleChange={ handleChange } 
+                    placeholder="Software Engineer"
+                    inputLabel="Role"
+                />
+
+                <button 
+                        style={
+                            {
+                                width: '80%',
+                                marginLeft: '10%',
+                                marginTop: '2%'
+                            }
+                        } 
+                        onClick={ ()=> {
+                            createApp(application) 
+                            toggleCreateApp();
+                        }}
+                        className="h-10 bg-selectgreen hover:bg-selectgreenhover transition-all rounded-md text-white cursor-pointer"
+                >
+                    Create Application
+                </button>
+
+            </div>
+        </div>
+    )
+}
+
+function ApplicationDetails({ details }){
+
     const [applicationHistory, modifyApplicationHistory] = useState({
         companyName: null,
         applicationStatus: null
+    });
+
+    const [applicationProgress] = useState({
+        pending: "Pending",
+        rejected: "Rejected",
+        offer: "Offer",
+        interview: "Interview"
     })
 
     const toggleApplicationDetails = useContext(applicationDetailsContext);
@@ -48,7 +118,7 @@ function ApplicationDetails(){
 
                 <ContainedInputField 
                     handleChange={ handleChange } 
-                    placeholder="Google..." 
+                    placeholder={ details.details.company }
                     inputLabel="Company Name"
                 />
 
@@ -63,7 +133,7 @@ function ApplicationDetails(){
                     <div className="w-full ">
                         <p className="text-gray-400 text-sm">Progress</p>
                      
-                        <Select handleChange={ handleChange } category="Progress"/>
+                        <Select handleChange={ handleChange } category="Progress" value={ applicationProgress[details.details.progress] }/>
                     </div>
                 </div>
                 <button 
@@ -84,16 +154,14 @@ function ApplicationDetails(){
     )
 }
 
-function Tools(){
+function Tools({ toggleCreateApp }){
+
     return (
         <div className="component-title relative flex items-center w-full h-10 mt-3">
-            <div className="cursor-pointer hover:bg-selectgreenhover transition-all flex items-center justify-center w-12 h-full ml-5">
-                <FilterIcon/>
-            </div>
             <div className="cursor-pointer hover:bg-selectgreenhover transition-all flex items-center justify-center w-12 h-full ml-2">
                 <ExportIcon/>
             </div>
-            <div className="cursor-pointer rounded-md hover:bg-selectgreenhover transition-all w-40 h-full ml-2 flex items-center">
+            <div onClick={ () => toggleCreateApp() } className="cursor-pointer rounded-md hover:bg-selectgreenhover transition-all w-40 h-full ml-2 flex items-center">
                 <div className="w-9 h-5/6 ml-2 flex justify-center items-center">
                     <PlusIcon/>
                 </div>
@@ -127,25 +195,52 @@ function TableHero(){
     )
 }
 
-function TableList(){
+function TableList(props){
     const toggleApplicationDetails = useContext(applicationDetailsContext);
+    const { deleteApp } = useContext(ApplicationsContext);
+    const [date, setDate] = useState(null);
+    const [time, setTime] = useState(null);
+
+    useEffect(() => {
+
+        function formatDate(date){
+            const createAt = date.split("T");
+            const fdate = createAt[0];
+            const ftime = createAt[1].split(".")[0];
+
+            return { fdate, ftime };
+        }
+
+        // format date 
+        const createdAt = formatDate(props.date);
+
+        // update state
+
+        setDate(createdAt.fdate);
+        setTime(createdAt.ftime);
+    }, []);
 
     return (
-        <div onClick={() => toggleApplicationDetails() } className="component-table-content-details cursor-pointer text-sm relative select-none flex items-center overflow-hidden transition-all hover:bg-coolgray w-full h-12">
+        <div onClick={() => toggleApplicationDetails({details: props}) } className="component-table-content-details cursor-pointer text-sm relative select-none flex items-center overflow-hidden transition-all hover:bg-coolgray w-full h-12">
         <div className="w-40 ml-5 flex items-center h-full">
-            <p1 className="text-gray-600">SovTech</p1>
+            <p1 className="text-gray-600">{ props.company }</p1>
         </div>
         <div className="w-40 flex items-center h-full">
-            <p1 className="p-1 text-gray-600">GR</p1>
+            <p1 className="p-1 text-gray-600">{ props.role }</p1>
         </div>
         <div className="w-40 flex items-center h-full">
-            <p1 className="text-gray-600">Pending</p1>
+            <p1 className="text-gray-600">{ props.progress }</p1>
         </div>
         <div className="w-40 flex items-center h-full">
-            <p1 className="text-gray-600">17 Aug</p1>
+            <p1 className="text-gray-600">{ date }</p1>
         </div>
         <div className="w-40 flex items-center h-full">
-            <p1 className="text-gray-600">14:43am</p1>
+            <p1 className="text-gray-600">{ time }</p1>
+        </div>
+        <div className="w-40 flex items-center h-full">
+            <div onClick={() => deleteApp(props.id) } className="w-1/2 h-full hover:bg-rose flex justify-center items-center">
+                <DeleteIcon/>
+            </div>
         </div>
     </div>
     )
@@ -153,37 +248,64 @@ function TableList(){
 
 
 function TableComponent(){
+    const user = useStore(state => state.about);
+    const { applications } = user;
+
+    const [applicationRecord ] = useState({
+        companyName: "SovTech",
+        role: "Graphic Designer",
+        progress: "pending"
+    });
+
     return (
         <div className="component-table relative w-full h-4/6 mt-3">
             <TableHero/>
-            <TableList/>
+            { applications.length ? applications.map(application => {
+                return <TableList
+                    company={ application.companyName }
+                    role={ application.role }
+                    id={ application.id }
+                    progress={ application.progress }
+                    date={ application.updatedAt }
+                />
+            }) : null }
         </div>
     )
 }
-const Applications = props => {
+const Applications = () => {
     const [applicationDetailsVisible, setApplicationVisibility] = useState(false);
+    const [currentApplicationDetails, setApplicationDetails] = useState("")
+    const [createApp, setCreateApp] = useState(false);
 
-    function toggleApplicationDetails(){
+    function toggleCreateApp(){
+        return setCreateApp(!createApp);
+    }
+
+    function toggleApplicationDetails(info){
+        setApplicationDetails(info);
         return setApplicationVisibility(!applicationDetailsVisible);
     }
 
     return (
-            <div className='None:container relative overflow-hidden h-screen bg-selectgray'>
-                <Navigation/>
-                <applicationDetailsContext.Provider value={ toggleApplicationDetails }>
-                <div style={
-                        {left: '20%'}
-                    } 
-                    className="container absolute w-4/5 h-full">
-                    <div className="component-title relative flex items-center w-full h-16 mt-10">
-                        <h1 className="text-white w-auto pointer-events-none select-none ml-5 text-4xl">Your applications</h1>
+            <ApplicationsProvider>
+                <div className='None:container relative overflow-hidden h-screen bg-selectgray'>
+                    <Navigation/>
+                    <applicationDetailsContext.Provider value={ toggleApplicationDetails }>
+                    <div style={
+                            {left: '20%'}
+                        } 
+                        className="container absolute w-4/5 h-full">
+                        <div className="component-title relative flex items-center w-full h-16 mt-10">
+                            <h1 className="text-white w-auto pointer-events-none select-none ml-5 text-4xl">Your applications</h1>
+                        </div>
+                        <Tools toggleCreateApp={ toggleCreateApp }/>
+                        { applicationDetailsVisible ? <ApplicationDetails details={ currentApplicationDetails } /> : null}
+                        <TableComponent/>
+                        { createApp ?  <CreateApplication toggleCreateApp={ toggleCreateApp }/> : null}
                     </div>
-                    <Tools/>
-                    { applicationDetailsVisible ? <ApplicationDetails/> : null}
-                    <TableComponent/>
+                    </applicationDetailsContext.Provider>
                 </div>
-                </applicationDetailsContext.Provider>
-            </div>
+            </ApplicationsProvider>
         );
 }
 
